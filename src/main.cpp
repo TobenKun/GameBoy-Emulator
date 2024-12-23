b54631b613906640e6ca5492aa307c0824b02dcf
TobenKun <zxcv1867@naver.com>
2024-12-24 00:50:40 +0900

    update git ignore

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,14 @@
+#include <iostream>
 #include "cpu/CPU.h"
 
 int main(int ac, char** av)
 {
+	if (ac != 2)
+	{
+		std::cerr << "Usage: " << av[0] << " <ROM>\n";
+		std::exit(1);
+	}
+
 	CPU cpu;
 
 	cpu.LoadROM(av[1]);
