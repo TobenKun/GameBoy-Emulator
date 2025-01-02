46e5a912aa818f929910429345e6a5cf68dd356b
TobenKun <zxcv1867@naver.com>
2025-01-02 22:00:31 +0900

    add instruction set

--- src/cpu/CPU.h
+++ src/cpu/CPU.h
@@ -2,6 +2,7 @@
 
 #include <array>
 #include <cstdint>
+#include <functional>
 #include <string>
 
 union Register
@@ -37,6 +38,7 @@ class CPU
   public:
 	std::array<uint8_t, 0x10000> memory;
 
+  private:	// Instruction
 	// Register
 	union
 	{
@@ -53,4 +55,42 @@ class CPU
 
 	uint16_t sp;
 	uint16_t pc;
+
+	uint8_t opcode;
+
+	// get 8-bit register
+	std::array<std::function<uint8_t&()>, 8> r8 = {
+		[&]() -> uint8_t& { return BC.lo; },
+		[&]() -> uint8_t& { return BC.hi; },
+		[&]() -> uint8_t& { return DE.lo; },
+		[&]() -> uint8_t& { return DE.hi; },
+		[&]() -> uint8_t& { return HL.lo; },
+		[&]() -> uint8_t& { return HL.hi; },
+		[&]() -> uint8_t& { return memory[HL.value]; },
+		[&]() -> uint8_t& { return A; },
+	};
+
+	// get 16-bit register
+	std::array<std::function<uint16_t&()>, 4> r16 = {
+		[&]() -> uint16_t& { return BC.value; },
+		[&]() -> uint16_t& { return DE.value; },
+		[&]() -> uint16_t& { return HL.value; },
+		[&]() -> uint16_t& { return sp; },
+	};
+
+	// get 16-bit register stack
+	std::array<std::function<uint16_t&()>, 4> r16stk = {
+		[&]() -> uint16_t& { return BC.value; },
+		[&]() -> uint16_t& { return DE.value; },
+		[&]() -> uint16_t& { return HL.value; },
+		[&]() -> uint16_t& { return AF.value; },
+	};
+
+	// get 16-bit register memory -> need to be done in seperate function
+	std::array<std::function<uint8_t&()>, 4> r16mem = {
+		[&]() -> uint8_t& { return memory[BC.value]; },
+		[&]() -> uint8_t& { return memory[DE.value]; },
+		[&]() -> uint8_t& { return memory[HL.value++]; },
+		[&]() -> uint8_t& { return memory[HL.value--]; },
+	};
 };
---
+++ src/cpu/instructions.cpp
@@ -0,0 +1,81 @@
+#include <cstdint>
+#include "CPU.h"
+
+// Cycle example
+//
+// void CPU::Cycle()
+// {
+// 	while (1)
+// 	{
+// 		opcode = memory[pc];
+// 		pc++; // pc 증가 먼저 한번 하고 들어간다
+// 		opcode_opcode; // 각자 함수로 점프
+// 	}
+// }
+
+void CPU::opcode_nope()
+{
+	// do nothing
+}
+
+void CPU::opcode_LD_r16_imm16()
+{
+	int dest = (opcode & 0x30) >> 4;
+
+	uint16_t& target = r16[dest]();
+	uint16_t  imm_byte = memory[pc++] << 8;
+	imm_byte |= memory[pc++];
+
+	target = imm_byte;
+}
+
+void CPU::opcode_LD_r16mem_A()
+{
+	int dest = (opcode & 0x30) >> 4;
+
+	uint8_t& target = r16mem[dest]();
+	target = A;
+}
+
+void CPU::opcode_LD_A_r16mem()
+{
+	int src = (opcode & 0x30) >> 4;
+
+	uint8_t& src_byte = r16mem[src]();
+	A = src_byte;
+}
+
+void CPU::opcode_LD_imm16_sp()
+{
+	uint16_t imm_byte = memory[pc++] << 8;
+	imm_byte |= memory[pc++];
+
+	memory[imm_byte] = sp & 0x0F;
+	memory[imm_byte + 1] = sp & 0xF0;
+}
+
+void CPU::opcode_INC_r16()
+{
+	uint16_t operand = (opcode & 0x38) >> 3;
+
+	uint16_t& target = r16[operand]();
+	target++;
+}
+
+void CPU::opcode_DEC_r16()
+{
+	uint16_t operand = (opcode & 0x38) >> 3;
+
+	uint16_t& target = r16[operand]();
+	target--;
+}
+
+void CPU::opcode_LD_r8_imm8()
+{
+	uint8_t operand = (opcode & 0x38) >> 3;
+	uint8_t imm_byte = memory[pc++];
+
+	uint8_t& target = r8[operand]();
+
+	target = imm_byte;
+}
